93720704cb0249765f4a7e1b7b7e5af994180c6b
Myster-Sneppi <60942937+Alex-ViGiL@users.noreply.github.com>
2022-04-30 21:15:30 +0300

    Add Menu Navigation
    Add menu navigation and images

---
+++ DistanceEducation(Windows)/Core/ObservableObjects.cs
@@ -0,0 +1,13 @@
+﻿namespace DistanceEducation_Windows_.Core
+{
+    class ObservableObjects : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
---
+++ DistanceEducation(Windows)/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,101 @@
+﻿namespace DistanceEducation_Windows_.ViewModels
+{
+    internal class MainWindowViewModel : ObservableObjects
+    {
+
+        #region Relays
+
+        public RelayCommand MainPageCommand { get; set; }
+        public RelayCommand HomeWorkPageCommand { get; set; }
+        public RelayCommand LessonPageCommand { get; set; }
+        public RelayCommand ChatsPageCommand { get; set; }
+        public RelayCommand TestsPageCommand { get; set; }
+        public RelayCommand GradesPageCommand { get; set; }
+        public RelayCommand GradesListPageCommand { get; set; }
+        public RelayCommand SettingsPageCommand { get; set; }
+        #endregion
+        #region Pages
+
+        MainPage MainPage { get; set; }
+        HomeWorksPage HomeWorkPage { get; set; }
+        LessonPage LessonPage { get; set; }
+        ChatsPage ChatsPage { get; set; }
+        TestsPage TestsPage { get; set; }
+        GradesPage GradesPage { get; set; }
+        GradesListPage GradesListPage { get; set; }
+        SettingsPage SettingsPage { get; set; }
+
+
+        #endregion
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            MainPage = new MainPage();
+            HomeWorkPage = new HomeWorksPage();
+            LessonPage = new LessonPage();
+            ChatsPage = new ChatsPage();
+            TestsPage = new TestsPage();
+            GradesPage = new GradesPage();
+            GradesListPage = new GradesListPage();
+            SettingsPage = new SettingsPage();
+
+
+            CurrentView = MainPage;
+
+            MainPageCommand = new RelayCommand(o =>
+            {
+                CurrentView = MainPage;
+            });
+
+            HomeWorkPageCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeWorkPage;
+            });
+
+            LessonPageCommand = new RelayCommand(o =>
+            {
+                CurrentView = LessonPage;
+            });
+
+            ChatsPageCommand = new RelayCommand(o =>
+            {
+                CurrentView = ChatsPage;
+            });
+
+            TestsPageCommand = new RelayCommand(o =>
+            {
+                CurrentView = TestsPage;
+            });
+
+            GradesPageCommand = new RelayCommand(o =>
+            {
+                CurrentView = GradesPage;
+            });
+
+            GradesListPageCommand = new RelayCommand(o =>
+            {
+                CurrentView = GradesListPage;
+            });
+
+            SettingsPageCommand = new RelayCommand(o =>
+            {
+                CurrentView = SettingsPage;
+            });
+
+        }
+
+    }
+}
