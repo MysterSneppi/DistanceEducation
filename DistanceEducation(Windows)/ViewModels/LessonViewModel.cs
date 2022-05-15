5cd068fd51ed809009dc6b0b49f88efb51f6a43f
Myster-Sneppi <60942937+Alex-ViGiL@users.noreply.github.com>
2022-05-15 21:45:44 +0300

    Edit Lesson View
    Edit Lesson View

---
+++ DistanceEducation(Windows)/ViewModels/LessonViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceEducation_Windows_.ViewModels
+{
+    internal class LessonViewModel : BaseViewModel
+    {
+        public List<LessonUserWindowView> list { get; set; }
+
+        
+
+        public LessonViewModel() 
+        {
+            list = new List<LessonUserWindowView>();
+
+            for (int i = 0; i < 10; i++) 
+            {
+                list.Add(new LessonUserWindowView());
+            }
+        }
+       
+
+    }
+}
